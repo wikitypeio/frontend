b28ecdd3c4d162220386897235e638593d611443
Brian Travis <bztravis88@gmail.com>
2024-01-13 00:07:19 -0500

    Fix searchWikipedia typing and add missing query check

--- utils/searchWikipedia.ts
+++ utils/searchWikipedia.ts
@@ -1,11 +1,6 @@
 import setSearchParams from '@/utils/setSearchParams';
 
-type SearchResult = {
-  description: string;
-  id: number;
-  thumbnail: { url: string };
-  title: string;
-};
+import type { SearchResult } from '@/utils/types';
 
 const LIMIT = 10;
 
@@ -15,16 +10,18 @@ const SEARCH_PATH = new URL(
 
 const searchWikipedia = async (query: string) => {
   try {
+    if (!query) throw 'No query';
+
     const url = new URL(SEARCH_PATH);
     setSearchParams(url, { q: query, limit: LIMIT });
 
     const res = await fetch(url);
     if (!res.ok) throw res.status;
-    const results: SearchResult = await res.json();
+    const { pages: results }: { pages: SearchResult[] } = await res.json();
 
     return results;
   } catch {
-    return null;
+    return [];
   }
 };
 
