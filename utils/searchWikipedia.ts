cfc4aab42e5a6818b119880ec80ec58149d3c3c5
Brian Travis <bztravis88@gmail.com>
2024-01-12 23:55:54 -0500

    Add searchWikipedia util

--- utils/searchWikipedia.ts
+++ utils/searchWikipedia.ts
@@ -1,3 +1,31 @@
-const SEARCH_URL = new URL(
-  'https://en.wikipedia.org/w/rest.php/v1/search/title?q=a&limit=10'
+import setSearchParams from '@/utils/setSearchParams';
+
+type SearchResult = {
+  description: string;
+  id: number;
+  thumbnail: { url: string };
+  title: string;
+};
+
+const LIMIT = 10;
+
+const SEARCH_PATH = new URL(
+  'https://en.wikipedia.org/w/rest.php/v1/search/title'
 );
+
+const searchWikipedia = async (query: string) => {
+  try {
+    const url = new URL(SEARCH_PATH);
+    setSearchParams(url, { q: query, limit: LIMIT });
+
+    const res = await fetch(url);
+    if (!res.ok) throw res.status;
+    const results: SearchResult = await res.json();
+
+    return results;
+  } catch {
+    return null;
+  }
+};
+
+export default searchWikipedia;
